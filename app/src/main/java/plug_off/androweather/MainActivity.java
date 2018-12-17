728058643b95c32c756ddae93ab6ed1aca363d28
Plug_OFF <lokilontan@yandex.ru>
2018-12-16 20:23:44 -0800

    -C/F switch implemented

--- app/src/main/java/plug_off/androweather/MainActivity.java
+++ app/src/main/java/plug_off/androweather/MainActivity.java
@@ -1,15 +1,19 @@
 package plug_off.androweather;
 
 import android.os.AsyncTask;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Weather W;
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,28 @@ public class MainActivity extends AppCompatActivity {
         new GetWeatherInBackground().execute(location.getText().toString());
     }
 
-    private class GetWeatherInBackground extends AsyncTask<String, Void, Weather> {
+    public void changeUnit(View v) {
+        Switch s = (Switch) findViewById(R.id.unit);
+        flag = !flag;
+        s.setChecked(flag);
+
+
+        if(s.isChecked()) {
+            TextView tempField = (TextView) findViewById(R.id.temperature);
+            tempField.setText("" + W.getTemperatureC(W.DynamicJson));
+        }
+        else {
+            TextView tempField = (TextView) findViewById(R.id.temperature);
+            tempField.setText("" + W.getTemperatureF(W.DynamicJson));
+        }
+    }
+
+
+
+   private class GetWeatherInBackground extends AsyncTask<String, Void, Weather> {
         @Override
         protected Weather doInBackground(String... zip){
-            Weather W = new Weather(zip[0]);
+            W = new Weather(zip[0]);
             W.fetch();
             return W;
         }
@@ -35,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Weather weather){
             TextView temp = (TextView) findViewById(R.id.temperature);
-            temp.setText("" + weather.getTemperatureF(weather.DynamicJson) + " / " + weather.getTemperatureC(weather.DynamicJson) );
+            temp.setText("" + weather.getTemperatureF(weather.DynamicJson) );
         }
     }
 
